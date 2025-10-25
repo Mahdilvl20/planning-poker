bdffee3fffbd8e7f3eb8785a7816beb1ff90e6d6
Mahdi <mahdiahmadibidani@gmail.com>
2025-10-25 22:40:42 +0330

    feat(store) : create pokerStore for room state control

---
+++ planning-poker-frontend/src/contex/pokerStore.ts
@@ -0,0 +1,19 @@
+import {create} from "zustand/react";
+
+export const pokerStore = create((set)=>({
+    players:[],
+    selectedCard:{},
+    revealed:false,
+    // @ts-ignore
+    selectCard:(playerId,card)=>
+        // @ts-ignore
+        set((state)=>({
+            selectedCard:{...state.selectedCard,[playerId]:card},
+        })),
+    revealeReasults:()=>set({revealed:true}),
+
+    resetRound:()=>({selectedCard:{},revealed:false}),
+
+
+
+}));
