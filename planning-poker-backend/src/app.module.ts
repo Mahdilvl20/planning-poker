08c13a34768256ac9fa36f749774fd3d978ab45b
Mahdi <mahdiahmadibidani@gmail.com>
2025-11-10 21:59:54 +0330

    connect nestjs to database

--- planning-poker-backend/src/app.module.ts
+++ planning-poker-backend/src/app.module.ts
@@ -1,15 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from './config/config.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { RoomsModule } from './rooms/rooms.module';
 import { MessagesModule } from './messages/messages.module';
 import { WebsocketsModule } from './websockets/websockets.module';
-
+import { ConfigModule } from '@nestjs/config';
+import {TypeOrmModule} from '@nestjs/typeorm';
 @Module({
-  imports: [ConfigModule, AuthModule, UsersModule, RoomsModule, MessagesModule, WebsocketsModule],
+  imports: [ConfigModule.forRoot({
+      isGlobal: true,
+  }), AuthModule, UsersModule, RoomsModule, MessagesModule, WebsocketsModule,TypeOrmModule.forRoot({
+      type: 'postgres',
+      host:process.env.HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+      autoLoadEntities: true,
+      synchronize: true,
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
